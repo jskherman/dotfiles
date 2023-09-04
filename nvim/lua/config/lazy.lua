e4d5e44e40e5719d6db0b9cca036acec14ba0a70
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-09-05 00:35:52 +0800

    add: extra plugins

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,8 +12,9 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.vscode" },
     -- import/override with your plugins
     { import = "plugins" },
   },
