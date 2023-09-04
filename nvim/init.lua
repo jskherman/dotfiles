08a01378859e70b724035ef314fe494fdfbdddac
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-09-05 00:36:12 +0800

    add: python path

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,6 @@ vim.o.shell = "pwsh"
 vim.o.shellcmdflag = "-Command"
 vim.o.shellquote = "\""
 vim.o.shellxquote = ""
+
+-- Set path to python3 executable for neovim
+vim.g.python3_host_prog = "C:\\Users\\Je Sian Keith Herman\\.pyenv\\pyenv-win\\versions\\3.10.11\\python.exe"
