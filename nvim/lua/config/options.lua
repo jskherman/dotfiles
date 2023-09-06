641041ab43249e57ef99a8e110dda6dbcef7d6a4
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-09-06 23:06:18 +0800

    fix: nvim typst-lsp not updating on type

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -51,14 +51,12 @@ vim.g.markdown_recommended_style = 0
 
 -- Set default shell for terminal to powershell
 vim.o.shell = "pwsh"
-vim.o.shellcmdflag = "-Command"
+vim.o.shellcmdflag = "-Command -NoLogo"
 vim.o.shellquote = "\""
 vim.o.shellxquote = ""
 
 -- Set path to python3 executable for neovim
 vim.g.python3_host_prog = "C:\\Users\\Je Sian Keith Herman\\.pyenv\\pyenv-win\\versions\\3.10.11\\python.exe"
--- Set path to typst executable
-vim.g.typst_cmd = "C:\\apps\\bin\\typst.exe"
 
 -- UltiSnips config
 vim.g.UltiSnipsExpandTrigger = '<tab>'
