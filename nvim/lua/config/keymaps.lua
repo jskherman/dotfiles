91a93c89705ce160a83e82c675805a0a231c3c46
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-09-07 11:10:42 +0800

    add: keymaps for moving lines up and down like in VS Code

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- This file is automatically loaded by lazyvim.config.init
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  --  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  vim.keymap.set(mode, lhs, rhs, options)
+end
+
+map("n", "<A-Up>", ":m -2<CR>") -- move line up(n)
+map("n", "<A-Down>", ":m +1<CR>") -- move line down(n)
+map("v", "<A-Up>", ":m -2<CR>", { desc = "Move line up" }) -- move line up(v)
+map("v", "<A-Down>", ":m +1<CR>", { desc = "Move line down" })
