5e251b35095994158a6ce49a843d2c2eaf52f055
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-09-07 22:16:10 +0800

    Add kanagawa, revert to tokyonight

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -11,9 +11,41 @@ return {
   },
 
   {
+    "rebelot/kanagawa.nvim",
+    enabled = false,
+    lazy = true,
+    name = "kanagawa",
+    opts = {
+      compile = false,  -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false,   -- do not set background color
+      dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = {             -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+      overrides = function(colors) -- add/modify highlights
+        return {}
+      end,
+      theme = "wave",  -- Load "wave" theme when 'background' option is not set
+      background = {   -- map the value of 'background' option to a theme
+        dark = "wave", -- try "dragon" !
+        light = "lotus"
+      },
+    }
+  },
+
+  {
     "catppuccin/nvim",
     lazy = true,
     name = "catppuccin",
+    enabled = false,
     opts = {
       integrations = {
         alpha = true,
@@ -50,7 +82,7 @@ return {
   -- {
   --   "LazyVim/LazyVim",
   --   opts = {
-  --     colorscheme = "catppuccin",
+  --     colorscheme = "kanagawa",
   --   },
   -- },
 }
