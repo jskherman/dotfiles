a431858e927db326a02314504673a8df07aec406
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-09-07 22:15:54 +0800

    add: latex lang support, project navigation

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,9 +12,11 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.tex" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.vscode" },
     -- import/override with your plugins
