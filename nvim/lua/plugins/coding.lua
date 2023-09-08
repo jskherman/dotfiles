2617b5bf53358038c555bf4cc354e2045880fa3c
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-09-08 12:56:01 +0800

    Simplify symbols outline

---
+++ nvim/lua/plugins/coding.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "simrat39/symbols-outline.nvim",
+        cmd = "SymbolsOutline",
+        keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+        opts = {
+            -- add your options that should be passed to the setup() function here
+            position = "right",
+        },
+    },
+}
