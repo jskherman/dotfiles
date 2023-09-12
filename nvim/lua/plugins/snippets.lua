2bf703455cf1726816e7cec4b871b57c5af83632
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-09-12 22:02:25 +0800

    fix: use UltiSnips over LuaSnips

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -1,60 +1,52 @@
 return {
-  -- Use <tab> for completion and snippets (supertab)
-  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
   {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
+    "SirVer/ultisnips",
+    dependencies = {
+      "honza/vim-snippets",
+    },
   },
-  -- then: setup supertab in cmp
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
-      "hrsh7th/cmp-emoji",
+      "quangnguyen30192/cmp-nvim-ultisnips",
+      "nvim-treesitter/nvim-treesitter",
+      config = function()
+        -- optional call to setup (see customization section)
+        require("cmp_nvim_ultisnips").setup {}
+      end,
+      -- If you want to enable filetype detection based on treesitter:
+      -- requires = { "nvim-treesitter/nvim-treesitter" },
     },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local luasnip = require("luasnip")
+    config = function()
+      local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
       local cmp = require("cmp")
 
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+      require("cmp").setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["UltiSnips#Anon"](args.body)
+          end,
+        },
+        sources = {
+          { name = "ultisnips" },
+          -- more sources
+        },
+        -- recommended configuration for <Tab> people:
+        mapping = {
+          ["<Tab>"] = cmp.mapping(
+            function(fallback)
+              cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
+            end,
+            { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+          ),
+          ["<S-Tab>"] = cmp.mapping(
+            function(fallback)
+              cmp_ultisnips_mappings.jump_backwards(fallback)
+            end,
+            { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+          ),
+        },
       })
     end,
-  },
-
-  {
-    "honza/vim-snippets",
-  },
-  {
-    "SirVer/ultisnips",
-  },
+  }
 }
