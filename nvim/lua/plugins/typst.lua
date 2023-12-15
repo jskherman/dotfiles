891ddc5adbc7e954795addd7d92bbbf7e8e20b58
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-12-15 13:44:31 +0800

    add: typst preview plugin

--- nvim/lua/plugins/typst.lua
+++ nvim/lua/plugins/typst.lua
@@ -1,7 +1,15 @@
 return {
-    {
-        'kaarmu/typst.vim',
-        ft = 'typst',
-        lazy = false,
-    },
+  {
+    "kaarmu/typst.vim",
+    ft = "typst",
+    lazy = false,
+  },
+  {
+    "chomosuke/typst-preview.nvim",
+    lazy = false, -- or ft = 'typst'
+    version = "0.1.*",
+    build = function()
+      require("typst-preview").update()
+    end,
+  },
 }
