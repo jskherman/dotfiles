327cb091e4924fed6338187cb5e0c4f135ec6d36
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-12-15 13:43:56 +0800

    add: typst watch

--- nvim/ftplugin/typst.lua
+++ nvim/ftplugin/typst.lua
@@ -2,12 +2,16 @@
 local Util = require("lazyvim.util")
 
 local function map(mode, lhs, rhs, opts)
-    local options = { noremap = true, silent = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    --  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-    vim.keymap.set(mode, lhs, rhs, options)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  --  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  vim.keymap.set(mode, lhs, rhs, options)
 end
 
-map("n", "<F5>", "<cmd>TypstWatch<CR>") -- Run TypstWatch
+-- map("n", "<F5>", "<cmd>TypstWatch<CR>") -- Run TypstWatch
+
+-- chomosuke/typst-preview.nvim
+map("n", "<F5>", "<cmd>TypstPreview<CR>") -- Run TypstWatch
+map("n", "<c-F5>", "<cmd>TypstPreviewStop<CR>") -- Run TypstWatch
