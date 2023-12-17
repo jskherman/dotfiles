0a1034298176b95b1c6ec8bc421ff1b05c2b050f
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-12-17 15:39:37 +0800

    clean: remove commented plugins

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,13 +1,4 @@
 return {
-  -- {
-  --   "simrat39/symbols-outline.nvim",
-  --   cmd = "SymbolsOutline",
-  --   keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-  --   opts = {
-  --     -- add your options that should be passed to the setup() function here
-  --     position = "right",
-  --   },
-  -- },
   -- Commenting
   {
     "echasnovski/mini.comment",
