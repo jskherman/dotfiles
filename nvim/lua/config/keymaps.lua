8dcd6b7791e294a43c7bb21b8d96f5312f5bd95b
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-12-17 15:38:48 +0800

    add: keybind for quickly fixing spelling errors

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -17,10 +17,23 @@ end
 -- Show filetype of current buffer
 map("n", "<leader>f?", '<cmd>echo "The file\'s type is: " .. &filetype<CR>', { desc = "Show filetype" })
 
-map("n", "<A-Up>", ":m -2<CR>", { desc = "Move line up" })     -- move line up(n)
+map("n", "<A-Up>", ":m -2<CR>", { desc = "Move line up" }) -- move line up(n)
 map("n", "<A-Down>", ":m +1<CR>", { desc = "Move line down" }) -- move line down(n)
-map("v", "<A-Up>", ":m -2<CR>", { desc = "Move line up" })     -- move line up (v)
+map("v", "<A-Up>", ":m -2<CR>", { desc = "Move line up" }) -- move line up (v)
 map("v", "<A-Down>", ":m +1<CR>", { desc = "Move line down" }) -- move line down (v)
+
+-- Ctrl + ; to automatically fix the nearest spelling error.
+map("i", "<C-;>", "<C-g>u<Esc>[s1z=`]a<C-g>u") -- <zg> to add custom words to spellfile
+
+-- Map hjkl to arrow keys
+map("n", "h", "<Left>", { noremap = true, silent = true })
+map("n", "j", "<Down>", { noremap = true, silent = true })
+map("n", "k", "<Up>", { noremap = true, silent = true })
+map("n", "l", "<Right>", { noremap = true, silent = true })
+
+-- ----------------------------------------------------------------------------
+
+-- ==== Plugins ====
 
 -- alpha.nvim Dashboard keymap
 map("n", "<leader>;", "<cmd>Alpha<CR>", { desc = "Dashboard" })
@@ -29,12 +42,7 @@ map("n", "<leader>;", "<cmd>Alpha<CR>", { desc = "Dashboard" })
 map("n", "<localleader>lw", "<cmd>VimtexCountWords<CR>", { desc = "Count words in doc" })
 map("n", "<localleader>l;", "<Cmd>update<CR><Cmd>VimtexCompileSS<CR>", { desc = "SS Compile" })
 
--- Map hjkl to arrow keys
-map("n", "h", "<Left>", { noremap = true, silent = true })
-map("n", "j", "<Down>", { noremap = true, silent = true })
-map("n", "k", "<Up>", { noremap = true, silent = true })
-map("n", "l", "<Right>", { noremap = true, silent = true })
-
 -- LuaSnip / Ultisnips keybindings
-map("n", "<leader>xs", "<cmd>lua require('luasnip').snippets={}; require('luasnip.loaders.from_lua').load({ paths = { './LuaSnip' } })<CR>", { desc = "Reload LuaSnip" } )
+map("n", "<leader>xs", "<cmd>lua require('luasnip').snippets={}; require('luasnip.loaders.from_lua').load({ paths = { './LuaSnip' } })<CR>", { desc = "Reload LuaSnip" })
 -- map("n", "<leader>xs", "<cmd>call UltiSnips#RefreshSnippets()<CR>", { desc = "Reload Ultisnips" })
+
